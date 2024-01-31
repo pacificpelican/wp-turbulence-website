106b183cfa7a2dd4da1b24bba2691629dda9deb0
Daniel McKeown <2019@sf3am.com>
2024-01-30 19:09:49 -0800

    simplify js code (revert to plugin version)

--- wp-turbulence.js
+++ wp-turbulence.js
@@ -1,18 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-  //  alert("script runs")
-
-  function randFont(mult = 100) {
-    return Math.random() * mult;
-  }
-
   function applyTurbulence(element) {
-    console.log("applying turbulence to " + element);
     const speed = Math.random() * 3 + 1; // Random speed between 1 and 4
     const delay = Math.random() * 100; // Random delay between 0 and 100ms
-    console.log("speed: " + speed + " delay: " + delay);
     element.style.animation = `shake ${speed}s infinite linear`;
     element.style.animationDelay = `${delay}ms`;
-}
+  }
 
   function traverseElements(element) {
     applyTurbulence(element);
